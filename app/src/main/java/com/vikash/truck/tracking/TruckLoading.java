d678738f7e2cb9cc399577dfe04c38cbad014e7d
Vikash <vikashkisku@gmail.com>
2020-07-05 10:40:32 +0530

    Registration and fetching of data according to qr code done

--- app/src/main/java/com/vikash/truck/tracking/TruckLoading.java
+++ app/src/main/java/com/vikash/truck/tracking/TruckLoading.java
@@ -1,16 +1,26 @@
 package com.vikash.truck.tracking;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.ml.md.R;
 
-public class TruckLoading extends AppCompatActivity {
+public class TruckLoading extends AppCompatActivity{
     private String StruckNo,StruckDriverName;
     private TextInputEditText truckNo,truckDriverName;
+    FirebaseFirestore db;
     @Override
     protected void onCreate(@Nullable Bundle bundle) {
         super.onCreate(bundle);
@@ -20,6 +30,23 @@ public class TruckLoading extends AppCompatActivity {
         StruckNo = getIntent().getStringExtra("truckNo");
         StruckDriverName = getIntent().getStringExtra("truckDriverName");
         truckNo.setText(StruckNo);
-        truckDriverName.setText(StruckDriverName);
+        db = FirebaseFirestore.getInstance();
+        // Create a reference to the cities collection
+        CollectionReference truckRef = db.collection("truck");
+
+// Create a query against the collection.
+        Query query = truckRef.whereEqualTo("truck_no", StruckNo);
+        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Log.d("DATABASE", document.getId() + " => " + document.getData());
+                        truckDriverName.setText(document.getString("truck_driver").toString());
+                    }
+                } else {
+                    Log.w("Database", "Error getting documents.", task.getException());
+                }
+            }});
     }
 }
